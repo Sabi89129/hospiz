1ff4a87dc59846ee228a240ab5d814c738be1ada
Sabi89129 <toffel4711@web.de>
2025-08-10 16:54:42 +0200

    reservix added

--- main.js
+++
@@ -1,6 +0,0 @@
-fetch('header-template.html')
-  .then(response => response.text())
-  .then(template => {
-    const headerContainer = document.getElementById('header-container');
-    headerContainer.innerHTML = template;
-  });
